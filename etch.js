e3f0b60bfddb4828a5552b6370ecef59cba77df6
cyril-hhf <cyrilhhf@gmail.com>
2024-08-19 17:44:17 +0800

    Grid is always square when size is changed

--- etch.js
+++ etch.js
@@ -7,9 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
     submitButton.addEventListener('click', () => {
         const inputValue = numberInput.value;
         gridContainer.innerHTML = '';
+
+        const itemSize = 800 / inputValue;
+        gridContainer.style.width = `${inputValue * itemSize}px`;
+        gridContainer.style.height = `${inputValue * itemSize}px`;
+
         for (let i = 1; i <= inputValue * inputValue; i++) {
             const gridItem = document.createElement('div');
             gridItem.className = 'grid-item';
+            gridItem.style.width = `${itemSize}px`;
+            gridItem.style.height = `${itemSize}px`;
             gridItem.addEventListener('mouseenter', () => {
                 gridItem.classList.add('hovered');
             });
