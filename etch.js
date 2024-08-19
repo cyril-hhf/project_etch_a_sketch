e3994e2212380a6266ade6b0ace10105b6f5600d
cyril-hhf <cyrilhhf@gmail.com>
2024-08-19 17:22:58 +0800

    Edited index.html & js & css file, users can choose grid size

--- etch.js
+++ etch.js
@@ -1,13 +1,20 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const gridContainer = document.getElementById('grid-container');
+    const submitButton = document.getElementById('submitButton');
+    const numberInput = document.getElementById('numberInput');
 
-    for (let i = 1; i <= 256; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.className = 'grid-item';
-        gridItem.addEventListener('mouseenter', () => {
-            gridItem.classList.add('hovered');
-        });
-        gridContainer.appendChild(gridItem);
-    }
+    submitButton.addEventListener('click', () => {
+        const inputValue = numberInput.value;
+        gridContainer.innerHTML = '';
+        for (let i = 1; i <= inputValue * inputValue; i++) {
+            const gridItem = document.createElement('div');
+            gridItem.className = 'grid-item';
+            gridItem.addEventListener('mouseenter', () => {
+                gridItem.classList.add('hovered');
+            });
+            gridContainer.appendChild(gridItem);
+        }
+    });
 });
+
